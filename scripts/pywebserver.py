8d58647a03d2cb943943c9b03c47df21877d3783
Kirsty Ellis <kirsty@shadowrobot.com>
2016-07-19 11:24:41 +0100

    template to generate html

--- scripts/pywebserver.py
+++ scripts/pywebserver.py
@@ -4,6 +4,7 @@ import SocketServer
 import socket
 import os
 from rospkg import RosPack
+import rospy
 
 rp = RosPack()
 frontend_path = rp.get_path('robot_blockly')
@@ -13,6 +14,9 @@ print("Changing serve path to: " + frontend_path)
 
 os.chdir(frontend_path)
 
+block_packages = rospy.get_param('block_packages')
+block_packages = ["/" + package for package in block_packages]
+
 HOST = socket.gethostname()
 PORT = 8000
 address = ("",PORT)
@@ -20,13 +24,13 @@ address = ("",PORT)
 
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
-        block_path = rp.get_path('sr_blockly_blocks')
-        if self.path.startswith('/sr_blockly_blocks'):
+        if self.path.startswith(tuple(block_packages)):
+            t = next(part for part in self.path.split(os.path.sep) if part)
+            block_path = rp.get_path(t)
             block_path = os.path.split(block_path)[0]
             os.chdir(block_path)
         else:
             os.chdir(frontend_path)
-        print self.path
         return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
 Handler = MyRequestHandler
