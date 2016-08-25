445ae83e5132716083d58be31fbad7dc300cf190
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2016-08-25 17:13:32 +0300

    Added JSON web service loader to call handlers in block libraries

--- frontend/pages/js/utils.js
+++ frontend/pages/js/utils.js
@@ -24,3 +24,35 @@ Blockly.readFile = function(file) {
     rawFile.send(null);
     return code;
 };
+
+
+/**
+ * Call web service.
+ * @param {string} file relative path to the file on the web server.
+ */
+Blockly.callWebService = function(module, method, parameters, asynchronous, success_callback, error_callback) {
+    $.ajax({
+        url: 'web_service_module',
+        type: 'post',
+        dataType: 'json',
+        async: asynchronous,
+        success: function(data, textStatus, jqXHR) {
+            if ('error' in data) {
+                if (error_callback) {
+                    error_callback(jqXHR, textStatus, data['error']);
+                }
+            }
+            else {
+                if (success_callback) {
+                    success_callback(data);
+                }
+            }
+        },
+        error: error_callback,
+        data: JSON.stringify({
+            module: module,
+            method: method,
+            parameters: parameters
+        })
+    });
+}
